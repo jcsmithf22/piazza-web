83ea3e683589aec89ba9705d98ffee8d829ed092
Josiah Smith <jcsmithf22@gmail.com>
2024-03-10 09:06:00 -0500

    Add test harness and write authentication tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -5,12 +5,7 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
 
   test "user is logged in and redirected to home with correct credentials" do
     assert_difference "@user.app_sessions.count", 1 do
-      post login_path params: {
-                        user: {
-                          email: "jerry@example.com",
-                          password: "password"
-                        }
-                      }
+      log_in @user
 
       assert_not_empty cookies[:app_session]
       assert_redirected_to root_path
@@ -18,12 +13,7 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "error is rendered for login with incorrect credentials" do
-    post login_path params: {
-                      user: {
-                        email: "wrong@example.com",
-                        password: "password"
-                      }
-                    }
+    log_in @user, password: "wrongpassword"
 
     assert_response :unprocessable_entity
     assert_select ".notification", I18n.t("sessions.create.incorrect-details")
