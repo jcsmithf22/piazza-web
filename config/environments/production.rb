d28bc3c307dd2992d43865355aca4540df4d1dec
Josiah Smith <jcsmithf22@gmail.com>
2024-03-11 23:35:26 -0500

    Fix config to use mailpace

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "piazza_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :resend
+  config.action_mailer.delivery_method = :mailpace
   config.action_mailer.default_url_options = {
     host: ENV["DOMAIN_NAME"],
     protocol: "https"
